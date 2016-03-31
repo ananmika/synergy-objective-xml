e440d81b94b374cc57d3dd56e8bf0999c99c226d
Arthur Rafayelyan <Arthur Rafayelyan>
2016-03-31 20:42:09 +0400

    Xml examples

---
+++ Arthur.Rafayelyan/XML/src/stax/Employee.java
@@ -0,0 +1,14 @@
+package stax;
+
+
+public class Employee{
+    String id;
+    String firstName;
+    String lastName;
+    String location;
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + "(" + id + ")" + location;
+    }
+}
